c8d802ca4eb8872c7c6e856823a38b13ef6b27b2
Ines <inesoubabas56@gmail.com>
2026-04-17 13:45:05 +0200

    Complete 0x02-dart-classes

---
+++ 0x02-dart-classes/4-main.dart
@@ -0,0 +1,10 @@
+import '4-mutable.dart';
+
+void main() {
+  final password = Password(password: "Passwordecode");
+  print(password.isValid());
+  print(password.toString());
+  password.password = "Youssef4321";
+  print(password.isValid());
+  print(password.toString());
+}
---
+++ 0x02-dart-classes/README.md
@@ -0,0 +1,3 @@
+# 0x02-dart-classes
+
+Project about Dart classes, constructors, JSON, private properties, getters, setters and inheritance.
